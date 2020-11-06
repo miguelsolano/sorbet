2385838df39f9d678d4cf86adfc310badc4be39e
Adam Procter <aprocter@stripe.com>
2020-11-05 17:13:48 -0800

    Better error message for &. on non-nilable receiver

--- core/types/calls.cc
+++ core/types/calls.cc
@@ -2132,6 +2132,14 @@ public:
     }
 } Magic_selfNew;
 
+class Magic_nilForSafeNavigation : public IntrinsicMethod {
+public:
+    void apply(const GlobalState &gs, DispatchArgs args, DispatchResult &res) const override {
+        ENFORCE(args.args.size() == 1);
+        res.returnType = Types::nilClass();
+    }
+} Magic_nilForSafeNavigation;
+
 class DeclBuilderForProcs_void : public IntrinsicMethod {
 public:
     void apply(const GlobalState &gs, DispatchArgs args, DispatchResult &res) const override {
@@ -2585,6 +2593,7 @@ const vector<Intrinsic> intrinsicMethods{
     {Symbols::Magic(), Intrinsic::Kind::Singleton, Names::callWithSplatAndBlock(), &Magic_callWithSplatAndBlock},
     {Symbols::Magic(), Intrinsic::Kind::Singleton, Names::suggestType(), &Magic_suggestUntypedConstantType},
     {Symbols::Magic(), Intrinsic::Kind::Singleton, Names::selfNew(), &Magic_selfNew},
+    {Symbols::Magic(), Intrinsic::Kind::Singleton, Names::nilForSafeNavigation(), &Magic_nilForSafeNavigation},
 
     {Symbols::DeclBuilderForProcsSingleton(), Intrinsic::Kind::Instance, Names::void_(), &DeclBuilderForProcs_void},
     {Symbols::DeclBuilderForProcsSingleton(), Intrinsic::Kind::Instance, Names::returns(),
